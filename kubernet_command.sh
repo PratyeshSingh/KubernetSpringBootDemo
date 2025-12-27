f035adf544778c335379568a3645cde0e865ae36
pratyesh <pratyesh3@gmail.com>
2025-12-27 09:56:35 +0530

    Deployment and Service yml file configuration

--- kubernet_command.sh
+++ kubernet_command.sh
@@ -35,32 +35,41 @@ alias kubectl="minikube kubectl --"
 minikube dashboard
 minikube stop
 
+# To show all deployed services in kubernet
+minikube service list
+
+# Here to START configuration for build
+
 #### info :- SPACE at end and (.) for current directory need to add otherwise provide the file directory instead
-#docker build -t springboot-kubernet-demo:latest .
-docker build -t springboot-kubernet-demo:1.0 .
+#docker build -t pratyesh3/springboot-kubernet-demo:1.0 .
+docker build -t pratyesh3/springboot-kubernet-demo:latest .
 
 
-#docker push springboot-kubernet-demo:latest .
-docker push springboot-kubernet-demo:1.0 .
+#docker push  pratyesh3/springboot-kubernet-demo:1.0
+docker push  pratyesh3/springboot-kubernet-demo:latest
 
 docker images
 
 # if yml/yaml file configuration are available else use ( kubectl create ) command below
-kubectl apply -f youFilename.yml
+#kubectl apply -f your_file_name.yml
+kubectl apply -f ./src/main/resources/application-service.yaml -f ./src/main/resources/application-deployment.yaml
+# to verify above setup
+kubectl get deployment
+kubectl get services
 
-#kubectl create deployment springboot-kubernet-demo --image=springboot-kubernet-demo:latest --port=8080
-kubectl create deployment springboot-kubernet-demo --image=springboot-kubernet-demo:1.0 --port=8080
+# in case of failing in deployment
+kubectl delete deployment springboot-kubernet-demo-deployment
 
-### in case of failing in deployment
-- kubectl delete deployment springboot-kubernet-demo
-- kubectl get deployment springboot-kubernet-demo
+# in case remove service from kubectl or minikube
+kubectl delete service springboot-kubernet-demo-service
 
-#minikube image load springboot-kubernet-demo:latest
-minikube image load springboot-kubernet-demo:1.0
-
-kubectl get deployment springboot-kubernet-demo
+kubectl get deployment springboot-kubernet-demo-deployment
 
 kubectl expose deployment springboot-kubernet-demo --type=NodePort
+
+# To load in minikube kubernet environment
+#minikube image load pratyesh3/springboot-kubernet-demo:1.0
+minikube image load pratyesh3/springboot-kubernet-demo:latest
 
 kubectl get service
 
